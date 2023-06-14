90624b9185b59ddd5f37dddf6f14591b69234bb8
arnob999 <arnob196@gmail.com>
2023-06-14 14:35:13 +0600

    new mongodb added

--- index.js
+++ index.js
@@ -33,7 +33,10 @@ function verifyJWT(req, res, next) {
     })
 }
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgfn0ly.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgfn0ly.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@iiuc.mo3glrj.mongodb.net/?retryWrites=true&w=majority`;
+
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 //new client nilam.....
 
